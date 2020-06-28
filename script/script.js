92d26579eb4fc30c2070d93751f98ee0993f93bf
Gaurav Raj Ghimire <grg.grg.47@gmail.com>
2020-06-28 19:39:39 -0400

    data extracted

--- script/script.js
+++ script/script.js
@@ -9,15 +9,116 @@ function getTemplate(TId) {
     return document.importNode(window.templates[TId], true)
 }
 
+let f1, f2
+Dropzone.autoDiscover = false;
+let opt = {
+    addRemoveLinks: true,
+    maxFiles: 1,
+    autoProcessQueue: false,
+    url: '/',
+    dictDefaultMessage: ""
+}
+class drzon {
+    constructor(name) {
+        opt.dictDefaultMessage = name == 'student' ? "Add Student File" : "Add Teacher File"
+        let zone = new Dropzone("#" + name, opt)
 
+        zone.on("addedfile", function(file) {
+            var r = new FileReader();
+            let col
+            r.onload = function(e) {
+                let data = e.target.result
+                let wb = XLSX.read(data, { type: 'binary' })
+                wb.SheetNames.forEach(sheet => {
+                    col = JSON.stringify(XLSX.utils.sheet_to_row_object_array(wb.Sheets[sheet]))
+                    if (name == 'student') {
+                        f1 = JSON.parse(col)
+                    } else {
+                        f2 = JSON.parse(col)
+                    }
 
+                });
+            }
+            r.readAsBinaryString(file);
+        })
+        zone.on("maxfilesexceeded", function(file) {
+            zone.removeAllFiles();
+            zone.addFile(file)
+        });
 
-window.addEventListener('load', (event) => {
-    document.querySelectorAll(".dz-button").forEach((element, index) => {
-        if(index == 0){
-            element.innerHTML = 'Drop Student\'s File'
-        }else{
-            element.innerHTML = 'Drop Teacher\'s File'
+        return zone
+    }
+}
+
+let stdzone = new drzon("student")
+let teazone = new drzon("teacher")
+
+document.getElementById("finder").addEventListener("click", e => {
+    let c = f1
+    console.log(c)
+    for (var key in c) {
+        if (c.hasOwnProperty(key)) {
+            let d = c[key]
+            // console.log(d)
+                // for (var key2 in d){
+                //     if (d.hasOwnProperty(key2)){
+                //         console.log(key2 + " -> " + d[key2])
+                //     } 
+                // }
         }
-    });
-});
+    }
+})
+
+// 
+
+// document.getElementById("stdfile").addEventListener("change", e => {
+//     f1 = e.target.files[0]
+// })
+
+// document.getElementById("teachfile").addEventListener("change", e => {
+//     f2 = e.target.files[0]
+// })
+
+// document.getElementById("finder").addEventListener("click", e => {
+
+//     if (f1 && f2) {
+
+//         var r1 = new FileReader();
+//         let col1, col2
+//         r1.onload = function(e) {
+//             let data = e.target.result
+//             let wb = XLSX.read(data, { type: 'binary' })
+//             wb.SheetNames.forEach(sheet => {
+//                 col1 = JSON.stringify(XLSX.utils.sheet_to_row_object_array(wb.Sheets[sheet]))
+//             });
+//         }
+//         r1.readAsBinaryString(f1);
+
+//         var r2 = new FileReader();
+//         r2.onload = function(e) {
+//             let data = e.target.result
+//             let wb = XLSX.read(data, { type: 'binary' })
+//             wb.SheetNames.forEach(sheet => {
+//                 col2 = JSON.stringify(XLSX.utils.sheet_to_row_object_array(wb.Sheets[sheet]))
+//             });
+//         }
+//         r2.readAsBinaryString(f2);
+
+//         setTimeout(() => {
+//             let c = JSON.parse(col1)
+//             for (var key in c) {
+//                 if (c.hasOwnProperty(key)) {
+//                     let d = c[key]
+//                     console.log(d)
+//                         // for (var key2 in d){
+//                         //     if (d.hasOwnProperty(key2)){
+//                         //         console.log(key2 + " -> " + d[key2])
+//                         //     } 
+//                         // }
+//                 }
+//             }
+
+//         }, 100);
+
+//     }
+// })
